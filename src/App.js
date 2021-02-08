3c5ca87d336e4cf21ab69ef1290f6d1c550a7f5a
Stiven <stiven.pinzon@correounivalle.edu.co>
2021-02-08 12:51:23 -0500

    adding login component(no validations)

--- src/App.js
+++ src/App.js
@@ -1,62 +1,67 @@
-import './App.css';
+import "./App.css";
 
-import React from 'react';
+import React from "react";
 
-import { MapContainer, TileLayer} from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-import L from 'leaflet';
-import jwt from 'jsonwebtoken';
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import jwt from "jsonwebtoken";
 
-import Marcadores from './Componentes/Marcadores'
-import { Component } from 'react';
+import Marcadores from "./Componentes/Marcadores";
+import { Component } from "react";
 
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 let DefaultIcon = L.icon({
   iconUrl: icon,
-  shadowUrl: iconShadow
+  shadowUrl: iconShadow,
 });
-
-
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
+class App extends Component {
+  state = {
+    marcadores: [],
+  };
 
-class App extends Component{
-
-  state ={
-        marcadores:[],
-      };
-
-    componentDidMount() {
-      var data1 = "";
-      fetch("http://localhost:3500/get_data",{ 
-      method: 'GET',
+  componentDidMount() {
+    var data1 = "";
+    fetch("http://localhost:3500/station", {
+      method: "GET",
       headers: {
-        Accept: 'application/json',
-                'Content-Type':'application/json',
-      }})// Aqui va la ruta
-       .then(res => res.json())
-        .then((data) => {
-          data1 = jwt.verify(data.token, "3ywg&hsnxu43o9+iaz&sdtr", function(err, dat){
-            return dat
-          })
-          this.setState({ marcadores : Object.entries(data1)[0][1]})
-       })
-        .catch(console.log)
-      };
-  
-  
-  render(){
-    return(
-          <MapContainer center={[4.08892, -76.20143]} zoom={13} scrollWheelZoom={true}>
-          <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' 
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-          <Marcadores marcadores={this.state.marcadores} />
-          </MapContainer>
-  );
-          
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }) // Aqui va la ruta
+      .then((res) => res.json())
+      .then((data) => {
+        data1 = jwt.verify(
+          data.token,
+          "3ywg&hsnxu43o9+iaz&sdtr",
+          function (err, dat) {
+            return dat;
+          }
+        );
+        this.setState({ marcadores: Object.entries(data1)[0][1] });
+      })
+      .catch(console.log);
+  }
+
+  render() {
+    return (
+      <MapContainer
+        center={[4.08892, -76.20143]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marcadores marcadores={this.state.marcadores} />
+      </MapContainer>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Dash from './Dashboard'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Dash from "./Dashboard";
+import Login from "./Componentes/Login";
 
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Dash />
+    <Login />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
