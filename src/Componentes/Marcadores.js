897b05fddad0e17b8807f8a6d56dd2254025a136
Stiven <stiven.pinzon@correounivalle.edu.co>
2021-04-08 13:39:54 -0500

    create user and sede, edit sede

--- src/Componentes/Marcadores.js
+++ src/Componentes/Marcadores.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import { Marker, Popup } from 'react-leaflet'
+import { Marker, Popup } from "react-leaflet";
+import EditSedeModal from "./EditSedeModal";
 
-const Marcadores= ({marcadores}) => {
-    return(
-        marcadores.map((marcador,i)=>(
-        <Marker key={i} position={[marcador.latitude , marcador.longitude]}>
-            <Popup>
-                <b>Nombre:</b> {marcador.name}. <br/>
-                <b>Direccion:</b> {marcador.address}. <br/>
-                <b>Telefono:</b> {marcador.phone}. <br/>
-            </Popup>
-        </Marker>       
-        ))    
-    ) 
+const Marcadores = ({ marcadores }) => {
+  return marcadores.map((marcador, i) => (
+    <Marker key={i} position={[marcador.latitude, marcador.longitude]}>
+      <Popup>
+        {console.log(marcador)}
+        <b>Nombre:</b> {marcador.name}. <br />
+        <b>Direccion:</b> {marcador.address}. <br />
+        <b>Telefono:</b> {marcador.phone}. <br />
+        <EditSedeModal id={marcador.id} />
+      </Popup>
+    </Marker>
+  ));
 };
 
-
-export default Marcadores
+export default Marcadores;
